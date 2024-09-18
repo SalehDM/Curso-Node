5da3d0764ffbced4885c26d84ac800947c9bb4c5
Saleh Daf Musa <paraelmovildes2@gmail.com>
2024-09-18 16:28:16 +0200

    Update: web server

--- 7WebServer/app.js
+++ 7WebServer/app.js
@@ -1,22 +1,39 @@
 const express = require('express')
+const hbs = require('hbs');
+
 const app = express()
 const port = 8080
 
 
+// Handlebar
+app.set('view engine', 'hbs')
+hbs.registerPartials( __dirname + '/views/partials');
+
+//Servir contenido estático
 app.use(express.static('public'));
 
-
-
-app.get('/hola-mundo', (req, res) => {
-    req.send('Hola mundo')
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'Aday',
+        title: 'Curso de Node'
+    })
 });
 
-app.get('*', (req, res) => {
-    req.send('404 | Page not found')
+app.get('/generic', (req, res) => {
+    res.render('generic', {
+        name: 'Aday',
+        title: 'Curso de Node'
+    })
 });
 
+app.get('/elements', (req, res) => {
+    res.render('elements', {
+        name: 'Aday',
+        title: 'Curso de Node'
+    })
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
